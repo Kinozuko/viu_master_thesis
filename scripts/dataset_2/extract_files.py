45ef4f5fc0d5e92fb3f38b9f1f8f64cba40960a2
Kurose <victorcmendozaa@gmail.com>
2024-09-27 22:41:35 +0200

    modify some comments

--- scripts/dataset_2/extract_files.py
+++ scripts/dataset_2/extract_files.py
@@ -5,8 +5,8 @@ import gzip
 root_folder = "datasets"
 dataset_2 = os.path.join(root_folder, "shifts_ms_pt2")
 best_folder = os.path.join(dataset_2, "shifts_ms_pt2/best/train")
-folders_to_keep = ['flair', 't1', 't2']  # Folders that contain specific types of images
-new_folder = "datasets/new_dataset"  # Folder to store the extracted files
+folders_to_keep = ['flair', 't1', 't2']  
+new_folder = "datasets/new_dataset"
 
 def extract_gz_file(gz_path, output_path):
     """
@@ -31,27 +31,21 @@ def extract_and_move():
     :param new_dataset_folder: Path to the new dataset folder where files will be organized.
     :return: None
     """
-    # Iterate over the folders (e.g., 'flair', 't1', 't2')
+    # Iterate over the folders (flair,t1,t2)
     for image_folder in folders_to_keep:
         print(f"Processing folder {image_folder}")
         
-        # Define the destination folder where extracted files will be moved
         move_to = os.path.join(new_folder, image_folder.capitalize())
         
-        # Get the source folder path where .gz files are located
         source_folder = os.path.join(best_folder, image_folder)
         
-        # Loop over the files in the source folder
         for image in os.listdir(source_folder):
             # Only process .gz files
             if image.endswith('.gz'):
-                # Construct the full path to the .gz file
                 image_path = os.path.join(source_folder, image)
                 
-                # Generate the output file name by removing the .gz extension
                 nii_file_name = os.path.splitext(image)[0]
                 
-                # Define the destination path for the extracted .nii file
                 output_path = os.path.join(move_to, nii_file_name)
                 
                 # Extract the .gz file and save the result in the destination folder
@@ -59,5 +53,4 @@ def extract_and_move():
                 print(f"Extracted {image} to {output_path}")
 
 if __name__ == '__main__':
-    # Run the extraction and file moving process
     extract_and_move()
