f3e3637c05842613b7e003f66e72161e5a2d33bb
Kurose <victorcmendozaa@gmail.com>
2024-10-02 22:18:53 +0200

    create new functions to combined and read new datasets

---
+++ scripts/dataset_selected/combined_datasets.py
@@ -0,0 +1,66 @@
+import tensorflow as tf
+from utils import parse_tfrecord_fn
+
+def load_tfrecord_with_label(file_path):
+    label = 0 if 'negative' in file_path else 1  
+    
+    raw_dataset = tf.data.TFRecordDataset(file_path)
+    parsed_dataset = raw_dataset.map(parse_tfrecord_fn)
+    
+    # Add label to the volume
+    labeled_dataset = parsed_dataset.map(lambda volume: (volume, label))  
+    return labeled_dataset
+
+def combine_datasets(positive_file, negative_file, buffer_size=8, batch_size=2):
+    # Load datasets with labels
+    positive_dataset = load_tfrecord_with_label(positive_file)
+    negative_dataset = load_tfrecord_with_label(negative_file)
+    
+    # Combine the positive and negative datasets
+    combined_dataset = positive_dataset.concatenate(negative_dataset)
+    
+    # Shuffle and batch the dataset
+    combined_dataset = combined_dataset.shuffle(buffer_size).batch(batch_size)
+    return combined_dataset
+
+def serialize_example(volume, label):
+    volume_tensor = tf.io.serialize_tensor(volume)
+    label_int = int(label)
+    feature = {
+        'volume': tf.train.Feature(bytes_list=tf.train.BytesList(value=[volume_tensor.numpy()])),
+        'label': tf.train.Feature(int64_list=tf.train.Int64List(value=[label_int]))
+    }
+    example_proto = tf.train.Example(features=tf.train.Features(feature=feature))
+    return example_proto.SerializeToString()
+
+def write_combined_tfrecord(dataset, file_path):
+    with tf.io.TFRecordWriter(file_path) as writer:
+        for batch_volumes, batch_labels in dataset: 
+            for volume, label in zip(batch_volumes, batch_labels):
+                print(batch_labels, label)
+                serialized_example = serialize_example(volume, label)
+                writer.write(serialized_example)
+
+if __name__ == '__main__':
+
+    train_dataset_combined = combine_datasets(
+        'datasets/train.tfrecord',
+        'datasets/train_negative.tfrecord')
+    val_dataset_combined = combine_datasets(
+        'datasets/val.tfrecord',
+        'datasets/val_negative.tfrecord')
+    test_dataset_combined = combine_datasets(
+        'datasets/test.tfrecord',
+        'datasets/test_negative.tfrecord')
+
+    write_combined_tfrecord(
+        train_dataset_combined,
+        'data/final_train.tfrecord')
+    write_combined_tfrecord(
+        val_dataset_combined,
+        'data/final_val.tfrecord')
+    write_combined_tfrecord(
+        test_dataset_combined, 
+        'data/final_test.tfrecord')
+
+    print("Final datasets saved as TFRecord files.")
--- scripts/dataset_selected/utils.py
+++ scripts/dataset_selected/utils.py
@@ -13,3 +13,23 @@ def load_tfrecord(file_path):
     raw_dataset = tf.data.TFRecordDataset(file_path)
     parsed_dataset = raw_dataset.map(parse_tfrecord_fn)
     return parsed_dataset
+
+def parse_combined_tfrecord_fn(example_proto):
+    feature_description = {
+        'volume': tf.io.FixedLenFeature([], tf.string),
+        'label': tf.io.FixedLenFeature([], tf.int64)
+    }
+    parsed_features = tf.io.parse_single_example(example_proto, feature_description)
+    
+    # Parse the volume tensor and the label
+    volume = tf.io.parse_tensor(parsed_features['volume'], out_type=tf.float32)
+    label = parsed_features['label']
+    return volume, label
+
+# Function to load the combined TFRecord files
+def load_combined_tfrecord(file_path, batch_size=2):
+    raw_dataset = tf.data.TFRecordDataset(file_path)
+    parsed_dataset = raw_dataset.map(parse_combined_tfrecord_fn)
+    parsed_dataset = parsed_dataset.batch(batch_size)
+    
+    return parsed_dataset
