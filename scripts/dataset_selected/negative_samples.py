e65c16f1207fc2fe488540a95162009729b49c5f
Kurose <victorcmendozaa@gmail.com>
2024-10-03 14:08:36 +0200

    add cardinality chec when creating negative samples

--- scripts/dataset_selected/negative_samples.py
+++ scripts/dataset_selected/negative_samples.py
@@ -6,6 +6,10 @@ if __name__ == '__main__':
     
     train, val, test = split_dataset(dataset)
 
+    print(f"Train dataset cardinality: {train.cardinality().numpy()}")
+    print(f"Test dataset cardinality: {val.cardinality().numpy()}")
+    print(f"Val dataset cardinality: {test.cardinality().numpy()}")
+    
     write_tfrecord(train, 'datasets/train_negative.tfrecord')
     write_tfrecord(val, 'datasets/val_negative.tfrecord')
     write_tfrecord(test, 'datasets/test_negative.tfrecord')
